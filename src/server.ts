23a1ada3fb08d2962ac9ec70c8aeb59e3142e9d7
alisonsantosofc <alisonsantoso22@gmail.com>
2022-02-24 16:30:40 -0300

    Create connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
+
+import './database';
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(9000, () => {
   console.log('✅️ Server started successfully on http://localhost:9000');
