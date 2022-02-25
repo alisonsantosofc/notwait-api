73d43ea9b0221dac19d4a433159b6f2652410fb8
alisonsantosofc <alisonsantoso22@gmail.com>
2022-02-25 15:25:15 -0300

    Create encrypted password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -15,7 +15,15 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    return response.json(user);
+    const responseUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(responseUser);
   } catch (err) {
     return response.status(400).json({ error: (err as Error).message });
   }
