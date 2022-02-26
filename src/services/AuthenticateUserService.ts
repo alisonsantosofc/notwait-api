24f51f8d3542185ad8bf3ec1df6fcb844a5d84e6
alisonsantosofc <alisonsantoso22@gmail.com>
2022-02-26 11:23:33 -0300

    Finished user authentication

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,47 @@
+import { compare } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '44c3cfcaa0248fa471aeb590a37f5d0e', {
+      subject: user.id,
+      expiresIn: '2 days',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
