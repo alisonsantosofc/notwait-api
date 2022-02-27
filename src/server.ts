1ba151ef3d5306bc5f4bdacdee9994ee8788dbca
alisonsantosofc <alisonsantoso22@gmail.com>
2022-02-27 15:30:19 -0300

    Add global error exception handling

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,34 @@
-import express from 'express';
 import 'reflect-metadata';
+import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
+
+import './database';
 
 import uploadConfig from './config/uploadFiles';
 import routes from './routes';
 
-import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'Error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'Error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(9000, () => {
   console.log('✅️ Server started successfully on http://localhost:9000');
